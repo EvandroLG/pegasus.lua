088f14d58ecaa3dab815e3d7a38dc6c90b1bc09c
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-11-13 14:52:36 -0200

    create webserver

---
+++ webserver.lua
@@ -0,0 +1,20 @@
+require 'socket'
+
+DEFAULT_ERROR_MESSAGE = [[
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+        "http://www.w3.org/TR/html4/strict.dtd">
+    <html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+        <title>Error response</title>
+    </head>
+    <body>
+        <h1>Error response</h1>
+        <p>Error code: %(code)d</p>
+        <p>Message: %(message)s.</p>
+        <p>Error code explanation: %(code)s - %(explain)s.</p>
+    </body>
+    </html>
+]]
+
+print(DEFAULT_ERROR_MESSAGE)
