367e910eb0c9680891d0db61a0c3e26586fa5de5
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-11-25 21:33:36 -0200

    little refactory

--- lib/request.lua
+++ lib/request.lua
@@ -20,15 +20,15 @@ Request.PATTERN_METHOD = '^(.*)%s'
 Request.PATTERN_PATH = '(.*)%s'
 Request.PATTERN_PROTOCOL = '(HTTP%/[0-9]%.[0-9])'
 Request.PATTERN_REQUEST = (Request.PATTERN_METHOD .. 
-  Request.PATTERN_PATH ..Request.PATTERN_PROTOCOL) 
+Request.PATTERN_PATH ..Request.PATTERN_PROTOCOL) 
 
 function Request:parseFirstLine()
     if (self.firstLine == nil) then
         self.firstLine = self.client:receive()
         -- Parse firstline http: METHOD PATH PROTOCOL, 
         -- GET Makefile HTTP/1.1 
-        local method, path, protocol= string.match(self.firstLine, 
-            Request.PATTERN_REQUEST)
+        local method, path, protocol = string.match(self.firstLine, 
+                                       Request.PATTERN_REQUEST)
             
         local filename, querystring = string.match(path, '^([^#?]+)(.*)')
         self._path = '.' .. path 
@@ -38,15 +38,18 @@ function Request:parseFirstLine()
 end
 
 Request.PATTERN_QUERY_STRING = '(%w)=(%w)'
-function Request:parse_url_encoded(value, _table)
+
+function Request:parseURLEncoded(value, _table)
     --value exists and _table is empty
-       if value and next(_table) == nil then
-         for k, v in  string.gmatch(value, Request.PATTERN_QUERY_STRING) do
-           _table[k] = v
-         end 
-       end
-       return _table   
+    if value and next(_table) == nil then
+        for k, v in  string.gmatch(value, Request.PATTERN_QUERY_STRING) do
+            _table[k] = v
+        end
+    end
+
+    return _table   
 end
+
 function Request:params()
     self:parseFirstLine()
     return self:parse_url_encoded(self._query_string, self._params)
@@ -71,29 +74,36 @@ function Request:headers()
         
     self:parseFirstLine()
     local data = self.client:receive()
+
     while (not (data == nil)) and (data:len() > 0)  do
-        local k , v =string.match(data, Request.PATTERN_HEADER)
-        if k and v then
-           self._headers[k] = v
+        local key, value = string.match(data, Request.PATTERN_HEADER)
+
+        if key and value then
+           self._headers[key] = value
         end
+
         data = self.client:receive()
     end
+
     self._headers_parsed = true
+
     return self._headers
 end
 
 function Request:body()
   self:headers()
   local data, err = self.client:receive()
+
   while (err == nil) and (not (data == nil)) do
       self._body = self._body .. data
       data = self.client:receive()
   end
+
   return self._body
 end
 
 function Request:form()
-  return self:parse_url_encoded(self:body(), self._form)
+  return self:parseURLEncoded(self:body(), self._form)
 end
 
 function Request:file()
