0f9568dcb7bb631521be8122e090f743b9af6c7c
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2015-02-15 12:20:07 -0200

    added some tests to pegasus module

---
+++ tests/test_pegasus.lua
@@ -0,0 +1,24 @@
+package.path = package.path .. ';../?.lua'
+local Pegasus = require 'lib/pegasus'
+
+
+describe('pegasus', function()
+  describe('instance', function()
+    it('should exists constructor to pegasus class', function()
+      local server = Pegasus:new()
+      assert.equal('table', type(server))
+    end)
+
+    it('should definer correct port', function()
+      local expectedPort = '6060'
+      local server = Pegasus:new(expectedPort)
+
+      assert.equal(expectedPort, server.port)
+    end)
+
+    it('should has standard port set', function()
+      local server = Pegasus:new()
+      assert.equal('9090', server.port)
+    end)
+  end)
+end)
