ddacfc90894f8ee23fc6b7ba5ec16172d87a5bb6
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-10-20 13:17:20 +0300

    Add. Support both lzlib and lua-zlib libraries.

--- spec/compress_spec.lua
+++ spec/compress_spec.lua
@@ -1,3 +1,21 @@
+local function write_file(f, d)
+  local f = assert(io.open(f, 'w+b'))
+  f:write(d)
+  f:close()
+end
+
+local function gzip(data)
+  write_file('test.gz', data)
+  local f = io.popen('gzip -d test.gz -c', 'r')
+  local result
+  if f then
+    result = f:read('*a')
+    f:close()
+  end
+  os.remove('test.gz')
+  return result
+end
+
 describe('compress #compress', function()
   local Compress = require 'pegasus.compress'
 
@@ -17,7 +35,7 @@ describe('compress #compress', function()
   end)
 
   describe('processBodyData', function()
-    function mockRequest()
+    local function mockRequest()
       return {
         headers = function() 
           return {
@@ -27,7 +45,16 @@ describe('compress #compress', function()
       }
     end
 
-    it('should add content-enconding = gzip if it is supported', function()
+    local function mockResponse()
+      return {
+        headers = {};
+        addHeader = function(self,k,v)
+          self.headers[k] = v
+        end
+      }
+    end
+
+    it('should not add content-enconding = gzip if it is not used', function()
       local request = mockRequest()
       local key = nil
       local value = nil
@@ -42,8 +69,8 @@ describe('compress #compress', function()
       local compress = Compress:new({ level=1 })
       compress:processBodyData('', false, request, response)
 
-      assert.equal(key, 'Content-Encoding')
-      assert.equal(value, 'gzip')
+      assert.is_nil(key)
+      assert.is_nil(value)
     end)
 
     it('should return data object when stayOpen is false', function()
@@ -53,6 +80,26 @@ describe('compress #compress', function()
 
       assert.equal(data, output)
     end)
+
+    it('should return data object when compress is not efficient', function()
+      local data = 'Hello from Pegasus'
+      local compress = Compress:new()
+      local response = mockResponse()
+      local output = compress:processBodyData(data, false, mockRequest(), response)
+
+      assert.equal(data, output)
+      assert.is_nil(response.headers['Content-Encoding'])
+    end)
+
+    it('should compress correct gzip format', function()
+      local data = ('a'):rep(2 * 1024)
+      local compress = Compress:new({ level = Compress.DEFAULT_COMPRESSION })
+      local response = mockResponse()
+      local output = compress:processBodyData(data, false, mockRequest(), response)
+      assert.equal(data, gzip(output))
+      assert.equal('gzip', response.headers['Content-Encoding'])
+    end)
+
   end)
 
 end)
--- src/pegasus/compress.lua
+++ src/pegasus/compress.lua
@@ -1,6 +1,84 @@
 local zlib = require "zlib"
 
-local Compress = {}
+local function zlib_name(zlib)
+  if zlib._VERSION and string.find(zlib._VERSION, 'lua-zlib', nil, true) then
+    return 'lua-zlib'
+  end
+
+  if zlib._VERSION and string.find(zlib._VERSION, 'lzlib', nil, true) then
+    return 'lzlib'
+  end
+end
+
+local z_lib_name = assert(zlib_name(zlib), 'Unsupported zlib Lua binding')
+
+local ZlibStream = {} do
+ZlibStream.__index = ZlibStream
+
+ZlibStream.NO_COMPRESSION      = zlib.NO_COMPRESSION       or  0
+ZlibStream.BEST_SPEED          = zlib.BEST_SPEED           or  1
+ZlibStream.BEST_COMPRESSION    = zlib.BEST_COMPRESSION     or  9
+ZlibStream.DEFAULT_COMPRESSION = zlib.DEFAULT_COMPRESSION  or -1
+ZlibStream.STORE               = 0
+ZlibStream.DEFLATE             = 8
+
+if z_lib_name == 'lzlib' then
+
+function ZlibStream:new(writer, level, method, windowBits)
+  level  = level or ZlibStream.DEFAULT_COMPRESSION
+  method = method or ZlibStream.DEFLATE
+
+  local o = setmetatable({
+    zd = assert(zlib.deflate(writer, level, method, windowBits));
+  }, self)
+
+  return o
+end
+
+function ZlibStream:write(chunk)
+  assert(self.zd:write(chunk))
+end
+
+function ZlibStream:close()
+  self.zd:close()
+end
+
+elseif z_lib_name == 'lua-zlib' then
+
+function ZlibStream:new(writer, level, method, windowBits)
+  level  = level or ZlibStream.DEFAULT_COMPRESSION
+  method = method or ZlibStream.DEFLATE
+
+  assert(method == ZlibStream.DEFLATE, 'lua-zlib support only deflated method')
+
+  local o = setmetatable({
+    zd = assert(zlib.deflate(level, windowBits));
+    writer = writer;
+  }, self)
+
+  return o
+end
+
+function ZlibStream:write(chunk)
+  chunk = assert(self.zd(chunk))
+  self.writer(chunk)
+end
+
+function ZlibStream:close()
+  local chunk = self.zd('', 'finish')
+  if chunk and #chunk > 0 then self.writer(chunk) end
+end
+
+end
+
+end
+
+local Compress = {} do
+
+Compress.NO_COMPRESSION      = ZlibStream.NO_COMPRESSION
+Compress.BEST_SPEED          = ZlibStream.BEST_SPEED
+Compress.BEST_COMPRESSION    = ZlibStream.BEST_COMPRESSION
+Compress.DEFAULT_COMPRESSION = ZlibStream.DEFAULT_COMPRESSION
 
 function Compress:new(options)
   local compress= {}
@@ -11,27 +89,31 @@ function Compress:new(options)
 end
 
 function Compress:processBodyData(data, stayOpen, request, response)
-  local accept_encoding = request:headers()['Accept-Encoding'] or ''
-  local accept_gzip = accept_encoding:find('gzip') ~= nil
+  local headers = request:headers()
+  local accept_encoding = headers and headers['Accept-Encoding'] or ''
+  local accept_gzip = not not accept_encoding:find('gzip', nil, true)
 
-  if not stayOpen and accept_gzip then
-    response:addHeader('Content-Encoding',  'gzip')
-
+  if not stayOpen and accept_gzip and self.options.level ~= ZlibStream.NO_COMPRESSION then
     local dataTable = {}
     local dataWrite = function (zdata)
        table.insert(dataTable, zdata)
     end
 
-    local stream = zlib.deflate(dataWrite,
-                   self.options.level or -1 , nil, 15 + 16)
-
+    local stream = ZlibStream:new(dataWrite, self.options.level, nil, 31)
     stream:write(data)
     stream:close()
 
-    return table.concat(dataTable)
-  else
-    return data
+    local compressed = table.concat(dataTable)
+
+    if #compressed < #data then
+      response:addHeader('Content-Encoding', 'gzip')
+      return compressed
+    end
   end
+
+  return data
+end
+
 end
 
 return Compress
