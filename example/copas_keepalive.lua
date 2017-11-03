cde32e3f397ba34297f1f951889b6621ccc10a30
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-11-03 10:29:27 +0300

    Fix. KeepAlive example.

--- example/copas_keepalive.lua
+++ example/copas_keepalive.lua
@@ -1,29 +1,42 @@
--- package.path = "./src/?.lua;./src/?/init.lua;"..package.path
+package.path = "./src/?.lua;./src/?/init.lua;"..package.path
 
 local Handler   = require 'pegasus.handler'
-local KeepAlive = require 'pegasus.handler'
+local KeepAlive = require 'pegasus.keepalive'
 local copas     = require 'copas'
 local socket    = require 'socket'
 
 local CopasKeepAlive = setmetatable({}, KeepAlive) do
 CopasKeepAlive.__index = CopasKeepAlive
 
-local function is_copas_socket_alive(s)
+local function is_socket_alive(s)
   return s and s.socket and s.socket.getfd and s.socket:getfd() ~= -1
 end
 
-function CopasKeepAlive:new(opt)
-  local o = KeepAlive.new(self, opt)
-
-  o._socket_test = is_copas_socket_alive
-
+local function clone(t)
+  local o = {}
+  for k,v in pairs(t) do
+    t[k] = v
+  end
   return o
+end
+
+function CopasKeepAlive:new(opt)
+  opt = opt and clone(opt) or {}
+  opt.socket_test = is_socket_alive
+  opt.socket_close = nil -- use default one
+
+  return KeepAlive.new(self, opt)
 end
 
 end
 
 local handler = Handler:new(function(req, rep)
   print(req, ' - precess')
+
+  -- to be able reuse request connection we have to read entire data bolong to this request
+  req:headers()
+  req:receiveBody()
+
   rep:statusCode(200)
   rep:write('Hello pegasus world!')
 end, nil, {
