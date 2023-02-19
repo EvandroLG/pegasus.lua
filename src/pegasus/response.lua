4d7112aed8576ef7a489fcbdfa844f9a71b3743f
Thijs Schreijer <thijs@thijsschreijer.nl>
2023-02-19 08:44:32 +0100

    chore(response) cleanup the error template

--- src/pegasus/response.lua
+++ src/pegasus/response.lua
@@ -65,19 +65,18 @@ local STATUS_TEXT = setmetatable({
 })
 
 local DEFAULT_ERROR_MESSAGE = [[
-  <!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.01//EN'
-      'http://www.w3.org/TR/html4/strict.dtd'>
-  <html>
-  <head>
-      <meta http-equiv='Content-Type' content='text/html;charset=utf-8'>
-      <title>Error response</title>
-  </head>
-  <body>
-      <h1>Error response</h1>
-      <p>Error code: {{ STATUS_CODE }}</p>
-      <p>Message: {{ STATUS_TEXT }}</p>
-  </body>
-  </html>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Error response</title>
+</head>
+<body>
+  <h1>Error response</h1>
+  <p>Error code: {{ STATUS_CODE }}</p>
+  <p>Message: {{ STATUS_TEXT }}</p>
+</body>
+</html>
 ]]
 
 local Response = {}
